ef4232b55f1b5ec91dbba4286105e4a7f8fe248b
pjenifer18 <pjenifer18@gmail.com>
2024-09-01 13:42:23 +0800

    initial commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Define a simple model (e.g., a User model)
-class User(db.Model):
+class AppUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -30,7 +30,7 @@ def index():
 @app.route('/users', methods=['POST'])
 def create_user():
     data = request.json
-    new_user = User(name=data['name'], email=data['email'])
+    new_user = AppUser(name=data['name'], email=data['email'])
     db.session.add(new_user)
     db.session.commit()
     return jsonify({'message': 'User created successfully'}), 201
@@ -38,14 +38,14 @@ def create_user():
 # Route to read all users
 @app.route('/users', methods=['GET'])
 def get_users():
-    users = User.query.all()
+    users = AppUser.query.all()
     return jsonify([{'id': user.id, 'name': user.name, 'email': user.email} for user in users])
 
 # Route to update a user
 @app.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
     data = request.json
-    user = User.query.get(id)
+    user = AppUser.query.get(id)
     if not user:
         return jsonify({'message': 'User not found'}), 404
     user.name = data['name']
@@ -56,7 +56,7 @@ def update_user(id):
 # Route to delete a user
 @app.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
-    user = User.query.get(id)
+    user = AppUser.query.get(id)
     if not user:
         return jsonify({'message': 'User not found'}), 404
     db.session.delete(user)
